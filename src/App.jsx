cd616711b24eb05b01eda953d7cedd5b7a612d1e
Cadu-f12 <cadudailha68@gmail.com>
2026-05-12 21:52:58 -0300

    feat: Create Home and BanhoTosa archive pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NavBar from "./components/Navbar"
+import Home from "./pages/Home.jsx"
+import Pages from "./pages/BanhoToda.jsx"
 
 function App() {
-  
+
   return (
     <>
-      
+      <BrowserRouter>
+        <header>
+          <div class="container">
+            <NavBar />
+          </div>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/banho-e-tosa" element={<BanhoTosa />}/>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </>
   )
 }
